72de41a531dedced45e98cc03f3631d8ccf5f942
Jalapina <davidpina14@gmail.com>
2019-02-19 21:15:53 -0600

    Styled it better

--- pokemon.js
+++ pokemon.js
@@ -2,35 +2,29 @@ $(document).ready(function(){
 
 
   for(var i=1; i<=151; i++){                                           //displays all 151 pokemon images
-         var pokeImg = "http://pokeapi.co/media/img/"+i+".png";
-         $('#pokemon').append("<img id="+i+" src="+pokeImg+">")
+         var pokeImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+i+".png";
+         $('.pokemon').append("<img id="+i+" src="+pokeImg+">")
     }
 
-    $('img').click(function(){
-
+    $('img').on("click tap",function(){
       var id = this.id;
 
-      var pokeurl='http://pokeapi.co/api/v2/pokemon/'+id+'/'  //url to pokemon info
-
-      var pokepic = 'http://pokeapi.co/media/img/'+id+'.png' //images for pokedox
+      var pokeurl='https://pokeapi.co/api/v2/pokemon/'+id  //url to pokemon info
+      var pokepic = 'https://raw.githubusercontent.com/Pokeapi/sprites/master/sprites/pokemon/'+id+'.png' //images for pokedox
 
       $.get(pokeurl, function(res){                        //Retrives info from pokeurl
 
-        $('#title').html("<h2>"+res.name+"</h2>");        //Sets pokemon name
+        $('.pokemon-name').html("<h2 class='name'>"+res.name+"</h2>");        //Sets pokemon name
         
-        $('#pokedox #dox').attr('src', pokepic);       //Sets pokemon img    
+        $('.pokedox-bio .pokemon-image').attr('src', pokepic);       //Sets pokemon img    
         
-        $('#pokedox .type').html("<h3>Type</h3><p>"+res.types[0].type.name+"</p>");  
+        $('.pokedox-bio .pokemon-type').html("<h3>Type</h3><p>"+res.types[0].type.name+"</p>");  
         
-        $('#pokedox #height').html("<p> Height: "+res.height+"</p>");     //Sets pokemons height
+        $('.pokedox-bio .pokemon-height').html("<p> Height: "+res.height+"</p>");     //Sets pokemons height
         
-        $('#pokedox #weight').html("<p> Wight: "+res.weight+"</p>");  //Sets pokemons weight
+        $('.pokedox-bio .pokemon-weight').html("<p> Wight: "+res.weight+"</p>");  //Sets pokemons weight
 
       }, 'json');
 
         });
 });
-
- $('img').hover(function(){
-          console.log('working')
-      });
